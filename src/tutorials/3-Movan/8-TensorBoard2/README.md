fa148e3f70e3430d590c5a323371901ba26fc6bf
David Lu <yungshenglu1994@gmail.com>
2019-03-29 23:38:58 +0800

    Add src/prectice/8-TensorBoard2

---
+++ src/tutorials/3-Movan/8-TensorBoard2/README.md
@@ -0,0 +1,32 @@
+# Practice 8 - Using TensorBoard 2
+
+This practice is refer to [莫烦PYTHON - TensorFlow: Tensorboard 可视化好帮手 1](https://morvanzhou.github.io/tutorials/machine-learning/tensorflow/4-1-tensorboard1/). Credits to [Morvan](https://github.com/MorvanZhou).
+
+---
+## Execution
+
+1. Run `main.py`
+    ```bash
+    # Make sure your current directory is in this folder
+    $ python3 main.py
+    ```
+2. If succeed, you will get the following result (the result is not unique)
+    ```bash
+    # Make sure your current directory is in this folder
+    $ ls ./logs/
+    events.out.tfevents.1553873751.yungshenglu
+    ```
+3. Run the log with TensorBoard
+    ```bash
+    $ tensorboard --logdir='./logs/'
+    TensorBoard 1.13.0 at http://yungshenglu:6006 (Press CTRL+C to quit)
+    ```
+4. Open the browser and navigate to the website show in your terminal (`http://0.0.0.0:6006`)
+    ![](res/tensorboard-demo.png)
+
+---
+## References
+
+* [TensorFlow Official - Tutorial](https://www.tensorflow.org/tutorials/)
+* [GitHub - tensorFlow/tensorflow](https://github.com/tensorflow/tensorflow)
+* [莫烦PYTHON - TensorFlow](https://morvanzhou.github.io/tutorials/machine-learning/tensorflow)
---
+++ src/tutorials/3-Movan/8-TensorBoard2/main.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def addLayer(inputs, in_size, out_size, name, activation=None):
+    # Define Weights and biases and add the name for TensorBoard
+    layer_name = 'layer%s' % name
+    with tf.name_scope(layer_name):
+        with tf.name_scope('weights'):
+            Weights = tf.Variable(tf.random_normal([in_size, out_size]), name='W')
+            tf.summary.histogram(layer_name + '/weights', Weights)
+        with tf.name_scope('biases'):
+            biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, name='b')
+            tf.summary.histogram(layer_name + '/biases', biases)
+        
+        # Define training function and add the name for TensorBoard
+        with tf.name_scope('y'):
+            y = tf.add(tf.matmul(inputs, Weights), biases)
+        
+        # Activation function
+        if activation is None:
+            outputs = y
+        else:
+            outputs = activation(y)
+            tf.summary.histogram(layer_name + '/outputs', outputs)
+        
+        return outputs
+
+
+def main():
+    ''' Create data '''
+    x = np.linspace(-1, 1, 300)[:, np.newaxis]
+    noise = np.random.normal(0, 0.05, x.shape)
+    y = np.square(x) - 0.5 + noise
+
+    # Add name for TensorBoard
+    with tf.name_scope('inputs'):
+        xs = tf.placeholder(tf.float32, [None, 1], name='x_input')
+        ys = tf.placeholder(tf.float32, [None, 1], name='y_input')
+
+    ''' Create TensorFlow model '''
+    layer1 = addLayer(xs, 1, 10, name=1, activation=tf.nn.relu)
+    prediction = addLayer(layer1, 10, 1, name=2, activation=None)
+
+    # Define loss function and optimizer
+    with tf.name_scope('loss'):
+        loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), reduction_indices=[1]))
+        tf.summary.scalar('loss', loss)
+    with tf.name_scope('train'):
+        train = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+    ''' Start training '''
+    with tf.Session() as sess:
+        merged = tf.summary.merge_all()
+        writer = tf.summary.FileWriter('./logs/', sess.graph)
+        # Initialize all variables in TensorFlow
+        sess.run(tf.global_variables_initializer())
+
+        # Train 1000 times and show summary every 50 times
+        for step in range(1000):
+            sess.run(train, feed_dict={
+                xs: x,
+                ys: y
+            })
+            if step % 50 == 0:
+                result = sess.run(merged, feed_dict={
+                    xs: x,
+                    ys: y
+                })
+                writer.add_summary(result, step)
+
+
+''' ENTRY POINT '''
+if __name__ == "__main__":
+    main()
